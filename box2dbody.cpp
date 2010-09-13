6e5fbac648297e4e4de67fac8c1dd637955cf0e2
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2010-09-13 17:18:55 +0200

    Remove mentioning of QmlArcade in the sources
    It's just a work-in-progress name, and the sources are two independent
    plugins.

--- box2dbody.cpp
+++ box2dbody.cpp
@@ -2,20 +2,20 @@
  * Box2D QML plugin
  * Copyright (C) 2010 Nokia Corporation
  *
- * This file is part of the QmlArcade.
+ * This file is part of the Box2D QML plugin.
  *
- * QmlArcade is free software; you can redistribute it and/or modify it
+ * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
  *
- * QmlArcade is distributed in the hope that it will be useful, but WITHOUT
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with QmlArcade.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "box2dbody.h"
