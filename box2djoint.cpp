qml-box2d/qml-box2d

commit 929198b2d0ce48f5c98b470f896a7f2063916d57

Set b2JointDef::bodyA/bodyB in Box2DJoint
Avoiding some more duplicated code.

box2djoint.cpp
@@ -37,6 +37,7 @@ Box2DJoint::Box2DJoint(b2JointDef &jointDef, QObject *parent) :
     mWorld(0),
     mJoint(0)
 {
+    mJointDef.userData = this;
 }
 
 Box2DJoint::~Box2DJoint()
@@ -111,12 +112,12 @@ void Box2DJoint::initialize()
         return;
     }
 
+    mJointDef.bodyA = bodyA()->body();
+    mJointDef.bodyB = bodyB()->body();
     mWorld = mBodyA->world();
     mJoint = createJoint();
-    if (mJoint) {
-        mJoint->SetUserData(this);
+    if (mJoint)
         emit created();
-    }
 }
 
 void Box2DJoint::componentComplete()

box2ddistancejoint.cpp
@@ -99,13 +99,10 @@ void Box2DDistanceJoint::setLocalAnchorB(const QPointF &localAnchorB)
 b2Joint *Box2DDistanceJoint::createJoint()
 {
     if (mAnchorsAuto) {
-        mDistanceJointDef.Initialize(bodyA()->body(),
-                                     bodyB()->body(),
-                                     bodyA()->body()->GetWorldCenter(),
-                                     bodyB()->body()->GetWorldCenter());
-    } else {
-        mDistanceJointDef.bodyA = bodyA()->body();
-        mDistanceJointDef.bodyB = bodyB()->body();
+        mDistanceJointDef.Initialize(mDistanceJointDef.bodyA,
+                                     mDistanceJointDef.bodyB,
+                                     mDistanceJointDef.bodyA->GetWorldCenter(),
+                                     mDistanceJointDef.bodyB->GetWorldCenter());
     }
 
     return world()->CreateJoint(&mDistanceJointDef);

box2dfrictionjoint.cpp
@@ -95,12 +95,9 @@ void Box2DFrictionJoint::setLocalAnchorB(const QPointF &localAnchorB)
 b2Joint *Box2DFrictionJoint::createJoint()
 {
     if (mAnchorsAuto) {
-        mFrictionJointDef.Initialize(bodyA()->body(),
-                                     bodyB()->body(),
-                                     bodyA()->body()->GetWorldCenter());
-    } else {
-        mFrictionJointDef.bodyA = bodyA()->body();
-        mFrictionJointDef.bodyB = bodyB()->body();
+        mFrictionJointDef.Initialize(mFrictionJointDef.bodyA,
+                                     mFrictionJointDef.bodyB,
+                                     mFrictionJointDef.bodyA->GetWorldCenter());
     }
 
     return world()->CreateJoint(&mFrictionJointDef);

box2dgearjoint.cpp
@@ -106,8 +106,6 @@ b2Joint *Box2DGearJoint::createJoint()
     if (!mJoint1->joint() || !mJoint2->joint())
         return 0;
 
-    mGearJointDef.bodyA = bodyA()->body();
-    mGearJointDef.bodyB = bodyB()->body();
     mGearJointDef.joint1 = mJoint1->joint();
     mGearJointDef.joint2 = mJoint2->joint();
 

box2dmousejoint.cpp
@@ -85,9 +85,6 @@ void Box2DMouseJoint::setTarget(const QPointF &target)
 
 b2Joint *Box2DMouseJoint::createJoint()
 {
-    mMouseJointDef.bodyA = bodyA()->body();
-    mMouseJointDef.bodyB = bodyB()->body();
-
     return world()->CreateJoint(&mMouseJointDef);
 }
 

box2dprismaticjoint.cpp
@@ -159,13 +159,11 @@ void Box2DPrismaticJoint::setLocalAnchorB(const QPointF &localAnchorB)
 b2Joint *Box2DPrismaticJoint::createJoint()
 {
     if (mAnchorsAuto) {
-        mPrismaticJointDef.Initialize(bodyA()->body(),
-                                      bodyB()->body(),
-                                      bodyA()->body()->GetWorldCenter(),
+        mPrismaticJointDef.Initialize(mPrismaticJointDef.bodyA,
+                                      mPrismaticJointDef.bodyB,
+                                      mPrismaticJointDef.bodyA->GetWorldCenter(),
                                       mPrismaticJointDef.localAxisA);
     } else {
-        mPrismaticJointDef.bodyA = bodyA()->body();
-        mPrismaticJointDef.bodyB = bodyB()->body();
         mPrismaticJointDef.referenceAngle = 0.0;
     }
 

box2dpulleyjoint.cpp
@@ -124,8 +124,6 @@ b2Joint *Box2DPulleyJoint::createJoint()
         qWarning() << "PulleyJoint: the joint length cannot be zero";
         return 0;
     }
-    mPulleyJointDef.bodyA = bodyA()->body();
-    mPulleyJointDef.bodyB = bodyB()->body();
 
     return world()->CreateJoint(&mPulleyJointDef);
 }

box2drevolutejoint.cpp
@@ -148,12 +148,9 @@ void Box2DRevoluteJoint::setLocalAnchorB(const QPointF &localAnchorB)
 b2Joint *Box2DRevoluteJoint::createJoint()
 {
     if (mAnchorsAuto) {
-        mRevoluteJointDef.Initialize(bodyA()->body(),
-                                     bodyB()->body(),
-                                     bodyA()->body()->GetWorldCenter());
-    } else {
-        mRevoluteJointDef.bodyA = bodyA()->body();
-        mRevoluteJointDef.bodyB = bodyB()->body();
+        mRevoluteJointDef.Initialize(mRevoluteJointDef.bodyA,
+                                     mRevoluteJointDef.bodyB,
+                                     mRevoluteJointDef.bodyA->GetWorldCenter());
     }
 
     return world()->CreateJoint(&mRevoluteJointDef);

commit 1cd5600ce23ebc297b9f3d66c6498e5ba5ee8f9a

Fixed issue with setting b2Joint::userData
The userData was always ending up as 0 and causing a crash on exit,
because the b2JointDef constructor was running after setting the
userData pointer. I broke this in change 929198b2d0ce48f5c98b470f8.

box2djoint.cpp
@@ -37,7 +37,6 @@ Box2DJoint::Box2DJoint(b2JointDef &jointDef, QObject *parent) :
     mWorld(0),
     mJoint(0)
 {
-    mJointDef.userData = this;
 }
 
 Box2DJoint::~Box2DJoint()
@@ -117,6 +116,7 @@ void Box2DJoint::initialize()
         return;
     }
 
+    mJointDef.userData = this;
     mJointDef.bodyA = bodyA()->body();
     mJointDef.bodyB = bodyB()->body();
     mWorld = mBodyA->world();