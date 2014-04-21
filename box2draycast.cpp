91ceda2fb1d489c4e57e0d14d0b16e4684b4217a
Thorbjørn Lindeijer <thorbjorn@lindeijer.nl>
2014-04-21 13:20:55 +0200

    Simplified raycast API
    * Added World.rayCast function, taking a RayCast object and the start
      and end points for the ray.
    
    * Removed RayCast.point1, RayCast.point2, RayCast.world, RayCast.repeat
      and RayCast.running. All these properties can be trivially implemented
      on the QML side.
    
    * Renamed RayCast.result to RayCast.maxFraction
    
    * Renamed RayCast.onCast to RayCast.onFixtureReported
    
    * Adjusted raycast example accordingly

--- box2draycast.cpp
+++ box2draycast.cpp
@@ -25,19 +25,12 @@
 
 #include "box2draycast.h"
 
-#include "box2dworld.h"
 #include "box2dfixture.h"
 
 
 Box2DRayCast::Box2DRayCast(QObject *parent) :
     QObject(parent),
-    mComponentComplete(false),
-    mPoint1(0,0),
-    mPoint2(0,0),
-    mRepeat(false),
-    mResult(Continue),
-    mRunning(false),
-    mWorld(0)
+    mMaxFraction(-1.0f)
 {
 }
 
@@ -46,129 +39,12 @@ float32 Box2DRayCast::ReportFixture(b2Fixture *fixture,
                                     const b2Vec2 &normal,
                                     float32 fraction)
 {
-    emit casted(toBox2DFixture(fixture),
-                toPixels(point),
-                toPixels(normal),
-                fraction);
+    mMaxFraction = -1.0f;
 
-    return mResult == Clip ? fraction : static_cast<qreal>(mResult);
-}
+    emit fixtureReported(toBox2DFixture(fixture),
+                         toPixels(point),
+                         toPixels(normal),
+                         fraction);
 
-void Box2DRayCast::classBegin()
-{
-}
-
-void Box2DRayCast::componentComplete()
-{
-    mComponentComplete = true;
-    if(mRunning) {
-        if(mRepeat)
-            startLoop(true);
-        else
-            cast();
-    }
-}
-
-void Box2DRayCast::cast()
-{
-    if(mWorld && mRunning) {
-        mWorld->world().RayCast(this,mPoint1,mPoint2);
-        if(!mRepeat)
-            setRunning(false);
-    }
-}
-
-void Box2DRayCast::startLoop(bool start)
-{
-    if(start)
-        connect(mWorld,SIGNAL(stepped()),SLOT(cast()));
-    else
-    {
-        mWorld->disconnect(this);
-        setRunning(false);
-    }
-}
-
-bool Box2DRayCast::repeat() const
-{
-    return mRepeat;
-}
-
-void Box2DRayCast::setRepeat(bool repeat)
-{
-    if(mRepeat != repeat) {
-        mRepeat = repeat;
-        if(mComponentComplete) {
-            startLoop(mRepeat);
-            emit repeatChanged();
-        }
-    }
-}
-
-Box2DRayCast::CastResult Box2DRayCast::result() const
-{
-    return mResult;
-}
-
-QPointF Box2DRayCast::point1() const
-{
-    return toPixels(mPoint1);
-}
-
-void Box2DRayCast::setPoint1(const QPointF &point1)
-{
-    if(this->point1() == point1)
-        return;
-    mPoint1 = toMeters(point1);
-    emit point1Changed();
-}
-
-QPointF Box2DRayCast::point2() const
-{
-    return toPixels(mPoint2);
-}
-
-void Box2DRayCast::setPoint2(const QPointF &point2)
-{
-    if(this->point2() == point2)
-        return;
-    mPoint2 = toMeters(point2);
-    emit point2Changed();
-}
-
-void Box2DRayCast::setResult(Box2DRayCast::CastResult result)
-{
-    mResult = result;
-}
-
-bool Box2DRayCast::running() const
-{
-    return mRunning;
-}
-
-void Box2DRayCast::setRunning(bool running)
-{
-    if(mRunning == running)
-        return;
-    mRunning = running;
-    if(mComponentComplete) {
-        if(mRepeat)
-            startLoop(mRunning);
-        else
-            cast();
-        emit runningChanged();
-    }
-}
-
-Box2DWorld *Box2DRayCast::world() const
-{
-    return mWorld;
-}
-
-void Box2DRayCast::setWorld(Box2DWorld *world)
-{
-    if(mWorld != world) {
-        mWorld = world;
-        emit worldChanged();
-    }
+    return mMaxFraction;
 }
