84a5a61e232fdfd0bccf0e9127bf25db6e139d08
MisterGC <serein.pfeiffer@gmail.com>
2019-08-04 11:32:50 +0200

    Demos: Refactor build cfg + source struct so that demos use real qml plugin.

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,16 +2,13 @@
 #include <QQmlApplicationEngine>
 #include <QDebug>
 #include <QtQml>
-#include <box2dplugin.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    Box2DPlugin plugin;
-    plugin.registerTypes("Box2D");
-
     QQmlApplicationEngine engine;
+    engine.addImportPath("plugins");
     engine.load(QUrl("qrc:/ui.qml"));
 
     return app.exec();
