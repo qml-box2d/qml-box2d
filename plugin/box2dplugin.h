c54d32d0fe51c66e13ad28a250cc8f9e8f5863d0
Johannes Wolf <j.wolf@klebert-engineering.de>
2020-10-16 11:41:14 +0200

    Honor CMake var QML_PLUGIN_LINK_TYPE

--- plugin/box2dplugin.h
+++ plugin/box2dplugin.h
@@ -34,9 +34,7 @@
 class Q_DECL_EXPORT Box2DPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-#if !defined(STATIC_PLUGIN_BOX2D)
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface")
-#endif
+    Q_PLUGIN_METADATA(IID "org.box2d.box2dqml")
 
 public:
     explicit Box2DPlugin(QObject *parent = 0);
